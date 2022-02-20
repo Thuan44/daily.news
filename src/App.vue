04c9d871dff4746a9bb4d0a33ac4dd8f46a7337e
Cong-Thuan Do <congthuan274@gmail.com>
2022-02-20 20:13:32 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <img alt="Vue logo" src="./assets/logo_large.png" />
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  background: #0d1217;
+}
+</style>
